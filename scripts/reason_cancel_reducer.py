415b36758cf7ea5a199bd3e0a7a474a3379ca306
omkarnaik713 <76861864+omkarnaik713@users.noreply.github.com>
2025-04-14 04:55:27 -0400

    reducer func

---
+++ scripts/reason_cancel_reducer.py
@@ -0,0 +1,19 @@
+import sys
+
+current_reason = None
+count = 0
+
+for line in sys.stdin:
+    reason, val = line.strip().split("\t")
+    val = int(val)
+
+    if current_reason == reason:
+        count += val
+    else:
+        if current_reason:
+            print(f"{current_reason}\t{count}")
+        current_reason = reason
+        count = val
+
+if current_reason:
+    print(f"{current_reason}\t{count}")
