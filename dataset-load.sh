293e9d09b440fd7593472fa108ede2095c9d59f3
Dylan Da Costa <dylan2222@gmail.com>
2025-04-19 19:49:26 -0400

    updated paths

--- dataset-load.sh
+++ dataset-load.sh
@@ -20,18 +20,18 @@ bash /home/ec2-user/Big-Data-Project/extract-decompress.sh
 
 EXTRACT_DIR="/tmp/airline_csvs"
 
-echo "===> Creating HDFS directory: /user/airline_data"
-sudo -u hdfs hdfs dfs -mkdir -p /user/airline_data
+echo "===> Creating HDFS directory: /user/airline_data/input"
+sudo -u hdfs hdfs dfs -mkdir -p /user/airline_data/input
 
 echo "===> Uploading CSV files to HDFS..."
 for csv_file in "$EXTRACT_DIR"/*.csv; do
   if [[ -f "$csv_file" ]]; then
     echo "Uploading $(basename "$csv_file") to HDFS..."
-    sudo -u hdfs hdfs dfs -put -f $csv_file /user/airline_data/
+    sudo -u hdfs hdfs dfs -put -f $csv_file /user/airline_data/input/
   fi
 done
 
 echo -e "\n===> Cleaning up local files..."
 rm -rf "$EXTRACT_DIR"
 
-echo -e "\n✅ All CSV files successfully uploaded to HDFS at /user/airline_data"
+echo -e "\n✅ All CSV files successfully uploaded to HDFS at /user/airline_data/input"
